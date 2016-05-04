2c229cf660b75ff9fe679ebcfa33a2857b6e13f7
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-05-03 23:19:34 -0400

    Remove accordion

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,5 @@
-import Accordion from './Accordion';
 import Header from './Header';
 import Footer from './Footer';
-
-if (document.querySelector('[data-accordion]')) {
-  for (let i = 0; i < document.querySelectorAll('[data-accordion]').length; i++) {
-    new Accordion(document.querySelectorAll('[data-accordion]')[i]);
-  }
-}
 
 new Header();
 new Footer();
